09985a78e908a9e135e8aee56b34bd35b2cfb4e3
frakibuddin <frakibuddin360@gmail.com>
2025-02-15 05:55:43 +0600

    udate

--- js/script.js
+++ js/script.js
@@ -10,3 +10,42 @@ window.onscroll = function () {
     header.classList.remove("active");
   }
 };
+
+//
+var listArray = ["BRING THE PARTY ANYWHERE", "AN ICON MEETS AN ICON"];
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  autoplayDisableOnInteraction: false,
+  slidesPerView: 1,
+  autoHeight: true,
+  autoplay: {
+    delay: 30000, //animation과 시간 맞춰줘야함
+  },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: "true",
+    type: "bullets",
+    renderBullet: function (index, className) {
+      return (
+        '<span class="' +
+        className +
+        '">' +
+        "<em>" +
+        listArray[index] +
+        "</em>" +
+        "<i></i>" +
+        "<b></b>" +
+        "</span>"
+      );
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
