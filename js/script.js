315bd435c199f7dbddb4524b44d65e28b258fd44
frakibuddin <frakibuddin360@gmail.com>
2025-02-17 01:36:51 +0600

    update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const header = document.querySelector("header");
 
 window.onscroll = function () {
+  handleScrollAnimation();
   // let posHight = document.documentElement.scrollHeight;
   let pos = window.scrollY;
   if (pos > 100) {
@@ -57,3 +58,42 @@ var mySwiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// onscroll animation
+// onscroll animation
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.15)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      //hideScrollElement(el);
+    }
+  });
+};
